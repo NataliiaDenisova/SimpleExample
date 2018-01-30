8140b4ab63f8a569de9a0a8b3a771d6ff6a2b66c
Natali <mshdni8@gmail.com>
2018-01-30 20:33:32 +0200

    30 Jan (1)

--- src/SeaGame/GameHelper.java
+++ src/SeaGame/GameHelper.java
@@ -1,12 +1,27 @@
 package SeaGame;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by Natali on 30.01.2018.
  */
 public class GameHelper {
 
-    String getUserInput(String str){
-        return str;
+    public String getUserInput(String prompt){
+
+        String inputLine = null;
+        System.out.print(prompt + " ");
+        try {
+            BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+            inputLine = is.readLine();
+            if (inputLine.length() == 0) return null;
+        } catch(IOException e) {
+            System.out.println("IOException: " + e);
+        }
+
+        return inputLine;
     }
 
 }
