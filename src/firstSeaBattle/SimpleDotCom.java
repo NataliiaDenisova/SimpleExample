fb686dcc992d1efde26f8ac88401b5a1cdecf61d
Natali <mshdni8@gmail.com>
2018-03-05 01:23:44 +0200

    02/03/18

---
+++ src/firstSeaBattle/SimpleDotCom.java
@@ -0,0 +1,28 @@
+package firstSeaBattle;
+
+/**
+ * Created by Natali on 05.03.2018.
+ */
+
+public class SimpleDotCom {
+    int[] locationCells;
+    int numOfHits = 0;
+
+    public String checkYourself(String userGuess){
+        int guess = Integer.parseInt(userGuess);
+        String result = "Мимо";
+
+        if(userGuess == locationCells[]){
+            numOfHits++;
+            //*
+        } else{
+
+        }
+        return "J";
+    }
+
+    public void setLocationCells(int[] cellLocations){
+
+    }
+
+}
---
+++ src/page_335/FullMoons.java
@@ -0,0 +1,16 @@
+package page_335;
+
+import java.util.Calendar;
+
+import static java.lang.System.out;
+
+/**
+ * Created by Natali on 02.03.2018.
+ */
+//p. 335
+public class FullMoons {
+    public static void main(String[] args) {
+        //Calendar c = new Calendar();
+    }
+}
+
